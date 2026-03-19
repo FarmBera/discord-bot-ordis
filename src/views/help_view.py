9f420648841ceaf8c71e20d0943449647ed51eaa
FarmBera <101335222+FarmBera@users.noreply.github.com>
2026-03-19 11:21:22 +0900

    Update: homepage url

--- src/views/help_view.py
+++ src/views/help_view.py
@@ -1,7 +1,7 @@
 from discord.ui import View, Button
 
-from src.translator import ts
 from config.TOKEN import HOMEPAGE, DONATION, SUPPORT_SERVER
+from src.translator import ts
 
 pf: str = "view.support-"
 
@@ -14,9 +14,9 @@ class SupportMasterView(View):
             Button(label=ts.get(f"{pf}support"), url=f"{SUPPORT_SERVER}", row=1)
         )
         self.add_item(Button(label=ts.get(f"{pf}help"), url=f"{HOMEPAGE}", row=2))
-        self.add_item(Button(label=ts.get(f"{pf}tos"), url=f"{HOMEPAGE}?id=tos", row=2))
+        self.add_item(Button(label=ts.get(f"{pf}tos"), url=f"{HOMEPAGE}/tos", row=2))
         self.add_item(
-            Button(label=ts.get(f"{pf}privacy"), url=f"{HOMEPAGE}/?id=privacy", row=2)
+            Button(label=ts.get(f"{pf}privacy"), url=f"{HOMEPAGE}/privacy", row=2)
         )
         self.add_item(Button(label=ts.get(f"{pf}donate"), url=DONATION, row=3))
 
@@ -25,9 +25,7 @@ class SupportView(View):
     def __init__(self):
         super().__init__(timeout=None)
 
-        self.add_item(
-            Button(label=ts.get(f"{pf}faq"), url=f"{HOMEPAGE}/?id=faq", row=1)
-        )
+        self.add_item(Button(label=ts.get(f"{pf}faq"), url=f"{HOMEPAGE}/faq", row=1))
         self.add_item(
             Button(label=ts.get(f"{pf}support"), url=f"{SUPPORT_SERVER}", row=1)
         )
