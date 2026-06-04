6a31d7eb9fda86aa907f0cfa24afccb70a27ce60
FarmBera <101335222+FarmBera@users.noreply.github.com>
2026-06-04 17:20:12 +0900

    refactor news: improve parser output & clean up code

--- src/parser/news.py
+++ src/parser/news.py
@@ -21,15 +21,11 @@ def w_news(newses, ts=_ts, lang=_default_lang):
     if not newses:
         return err_embed("news")
 
-    # idx: int = 0
-    output_msg: str = ""
+    output_msg: str = f"# {ts.get(f'{pf}title')}\n"
+    game_news = []
+    community_news = []
 
-    output_msg += f"# {ts.get(f'{pf}title')}\n\n"
-
-    news_news = []
-    news_community = []
-
-    # process
+    # categorize news
     for item in newses:
         t_id = item["_id"]
         try:
@@ -49,46 +45,48 @@ def w_news(newses, ts=_ts, lang=_default_lang):
         for t_title in item["Messages"]:
             if item.get("Community") and t_title["LanguageCode"] == Lang.EN:
                 t_title = getLanguage(t_title["Message"])
-                news_community.append(
+                community_news.append(
                     News(nid=t_id, date=t_date, title=t_title, url=t_url, img=t_img)
                 )
                 continue
 
             if t_title["LanguageCode"] == lang:
                 t_title = getLanguage(t_title["Message"])
-                news_news.append(
+                game_news.append(
                     News(nid=t_id, date=t_date, title=t_title, url=t_url, img=t_img)
                 )
 
     # reverse list
-    news_news = news_news[::-1]
-    news_community = news_community[::-1]
+    game_news = game_news[::-1]
+    community_news = community_news[::-1]
 
-    if news_news:
-        output_msg += f"## {ts.get(f'{pf}ingame')}\n\n"
-        for item in news_news:
+    # create news text
+    if game_news:
+        output_msg += f"## {ts.get(f'{pf}ingame')}\n"
+        for item in game_news:
+            output_msg += f"- [{item.title}]({item.url})\n"
+    if community_news:
+        output_msg += f"## {ts.get(f'{pf}comu')}\n"
+        for item in community_news:
             output_msg += f"- [{item.title}]({item.url})\n"
 
-    if news_community:
-        output_msg += f"\n## {ts.get(f'{pf}comu')}\n\n"
-        for item in news_community:
-            output_msg += f"- [{item.title}]({item.url})\n"
-
-        # idx += 1
-        # if idx >= LIMIT_OUTPUT_CNT:
-        #     break
-
+    # create discord embed
     embed = discord.Embed(
         description=output_msg,
-        color=0x00FFFF,
+        color=discord.Color.darker_grey(),
     )
-    # set embed thumb
+    # set embed thumbnail img
     img_url = None
-    if news_news:
-        img_url = news_news[0].img
-    elif news_community:
-        img_url = news_community[0].img
+    if game_news:
+        img_url = game_news[0].img
+    elif community_news:
+        img_url = community_news[0].img
     if img_url:
         embed.set_image(url=img_url)
 
     return embed
+
+
+# from src.utils.data_manager import get_obj
+# from src.constants.keys import NEWS
+# print(w_news(get_obj(NEWS)).description)
